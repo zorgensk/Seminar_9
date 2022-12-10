9cfa0e1af516bff1f7133acdc3021ef445db330f
zorgensk <zorgensk@gmail.com>
2022-12-10 20:18:19 +0700

    Homework1

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -5,15 +5,14 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-int Func(int n)
+int Func(int m, int n)
 {
-    if (n == 0)
-        return 1;
-    Console.WriteLine(n);
-        return Func(n - 1);
+     if(m > n) return n;
+    Console.Write(m+" ");
+    return Func(m + 1, n) ;
 }
 
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-Func(n);
+Func(1,n);
